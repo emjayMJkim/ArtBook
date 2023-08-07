189f02ddcd098b01e7b4384a51243bb549846190
emjayMJkim <minjeong030420@gmail.com>
2023-08-08 03:09:31 +0900

    [FEAT] BaseActivity&Fragment 생성

---
+++ app/src/main/java/com/example/artbook/base/BaseFragment.kt
@@ -0,0 +1,32 @@
+package com.example.artbook.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<B : ViewDataBinding>(@LayoutRes private val layoutResId: Int) :
+    Fragment() {
+    private var _binding: B? = null
+    val binding
+        get() = requireNotNull(_binding) { "${this::class.java.simpleName}에서 에러가 발생" }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View {
+        _binding = DataBindingUtil.inflate(inflater, layoutResId, container, false)
+        binding.lifecycleOwner = viewLifecycleOwner
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        _binding = null
+        super.onDestroyView()
+    }
+}
