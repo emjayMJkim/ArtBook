7bd96c060a8145199e88aad58b40b2eb8e08e279
emjayMJkim <minjeong030420@gmail.com>
2023-08-09 03:30:13 +0900

    [CHORE] activity 테스트

--- app/src/main/java/com/example/artbook/presentation/main/MainActivity.kt
+++ app/src/main/java/com/example/artbook/presentation/main/MainActivity.kt
@@ -1,4 +1,12 @@
 package com.example.artbook.presentation.main
 
-class MainActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.artbook.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
 }
