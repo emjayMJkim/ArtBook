4bcc9bbb19eae6427a4fdac5d55e7e53b8bfba7b
emjayMJkim <minjeong030420@gmail.com>
2023-08-10 02:46:27 +0900

    [UI] artFragment 기본 뷰 그리기

--- app/src/main/java/com/example/artbook/presentation/home/ArtFragment.kt
+++ app/src/main/java/com/example/artbook/presentation/home/ArtFragment.kt
@@ -1,4 +1,10 @@
 package com.example.artbook.presentation.home
 
-class ArtFragment {
+import com.example.artbook.R
+import com.example.artbook.base.BaseFragment
+import com.example.artbook.databinding.FragmentArtBinding
+
+class ArtFragment : BaseFragment<FragmentArtBinding>(R.layout.fragment_art) {
+
+
 }
