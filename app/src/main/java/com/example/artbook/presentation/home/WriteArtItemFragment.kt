707c9bdd9ec9cb97e659a1a46354e2f04903eb70
emjayMJkim <minjeong030420@gmail.com>
2023-08-10 03:58:33 +0900

    [UI] item 동적생성용 fragment 추가

---
+++ app/src/main/java/com/example/artbook/presentation/home/WriteArtItemFragment.kt
@@ -0,0 +1,8 @@
+package com.example.artbook.presentation.home
+
+import com.example.artbook.R
+import com.example.artbook.base.BaseFragment
+import com.example.artbook.databinding.FragmentWriteArtItemBinding
+
+class WriteArtItemFragment : BaseFragment<FragmentWriteArtItemBinding>(R.layout.fragment_write_art_item) {
+}
