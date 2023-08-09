43f00e209567684e824b2b3c6e5f57bae4eb56ad
emjayMJkim <minjeong030420@gmail.com>
2023-08-10 04:21:13 +0900

    [UI] api 통신용 fragment 생성

---
+++ app/src/main/java/com/example/artbook/presentation/home/SearchImgFragment.kt
@@ -0,0 +1,8 @@
+package com.example.artbook.presentation.home
+
+import com.example.artbook.R
+import com.example.artbook.base.BaseFragment
+import com.example.artbook.databinding.FragmentSearchImgBinding
+
+class SearchImgFragment : BaseFragment<FragmentSearchImgBinding>(R.layout.fragment_search_img) {
+}
