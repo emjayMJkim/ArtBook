b0cd1c57846da09a6f2c82d5ba46d69e88d987a6
emjayMJkim <minjeong030420@gmail.com>
2023-08-12 04:58:48 +0900

    [ADD] writeArtItemFragment popBackStack() 추가

--- app/src/main/java/com/example/artbook/presentation/home/WriteArtItemFragment.kt
+++ app/src/main/java/com/example/artbook/presentation/home/WriteArtItemFragment.kt
@@ -2,12 +2,14 @@ package com.example.artbook.presentation.home
 
 import android.os.Bundle
 import android.view.View
+import androidx.activity.OnBackPressedCallback
 import androidx.navigation.fragment.findNavController
 import com.example.artbook.R
 import com.example.artbook.base.BaseFragment
 import com.example.artbook.databinding.FragmentWriteArtItemBinding
 
 class WriteArtItemFragment : BaseFragment<FragmentWriteArtItemBinding>(R.layout.fragment_write_art_item) {
+
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -17,5 +19,18 @@ class WriteArtItemFragment : BaseFragment<FragmentWriteArtItemBinding>(R.layout.
                 WriteArtItemFragmentDirections.actionWriteArtItemFragmentToSearchImgFragment(),
             )
         }
+
+        callback()
     }
+
+    private fun callback() {
+        val callback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+
+        requireActivity().onBackPressedDispatcher.addCallback(callback)
+    }
+
 }
