6f51a53121e06ceef28fb529816bade1148c33e8
emjayMJkim <minjeong030420@gmail.com>
2023-08-14 03:37:41 +0900

    [ADD] base.url&api.key 상수화

---
+++ app/src/main/java/com/example/artbook/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.example.artbook.util
+
+import com.example.artbook.BuildConfig
+
+object Constants {
+
+    const val BASE_URL = BuildConfig.ARTBOOK_BASE_URL
+    const val API_KEY = BuildConfig.API_KEY
+}
