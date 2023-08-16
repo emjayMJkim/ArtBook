21c2ed4becd3f96df9bf43785580e4ccdff5c398
emjayMJkim <minjeong030420@gmail.com>
2023-08-17 00:29:53 +0900

    [MOD] fragmentFactory 파일명 수정&코드 추가

--- app/src/main/java/com/example/artbook/util/FragmentFactory.kt
+++ app/src/main/java/com/example/artbook/util/FragmentFactory.kt
@@ -3,15 +3,19 @@ package com.example.artbook.util
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentFactory
 import com.bumptech.glide.RequestManager
-import com.example.artbook.presentation.home.WriteArtItemFragment
+import com.example.artbook.presentation.home.*
 import javax.inject.Inject
 
-class ArtFragmentFactory @Inject constructor(
+class FragmentFactory @Inject constructor(
+    private val artAdapter: ArtAdapter,
+    private val imageAdapter: ImageAdapter,
     private val glide: RequestManager,
 ) : FragmentFactory() {
 
     override fun instantiate(classLoader: ClassLoader, className: String): Fragment {
         return when (className) {
+            ArtFragment::class.java.name -> ArtFragment(artAdapter)
+            SearchImgFragment::class.java.name -> SearchImgFragment(imageAdapter)
             WriteArtItemFragment::class.java.name -> WriteArtItemFragment(glide)
             else -> super.instantiate(classLoader, className)
         }
