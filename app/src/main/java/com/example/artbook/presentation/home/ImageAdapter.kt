a8c8cf041fea4e4e4e21681afe3f9d9b75bff84c
emjayMJkim <minjeong030420@gmail.com>
2023-08-17 00:29:14 +0900

    [ADD] ImageAdapter 생성

---
+++ app/src/main/java/com/example/artbook/presentation/home/ImageAdapter.kt
@@ -0,0 +1,64 @@
+package com.example.artbook.presentation.home
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.RequestManager
+import com.example.artbook.R
+import javax.inject.Inject
+
+class ImageAdapter @Inject constructor(
+    val glide: RequestManager,
+) : RecyclerView.Adapter<ImageAdapter.ImageViewHolder>() {
+
+    class ImageViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    private var onItemClickListener: ((String) -> Unit) ? = null
+
+    private val diffUtil = object : DiffUtil.ItemCallback<String>() {
+        override fun areItemsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: String, newItem: String): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val recyclerListDiffer = AsyncListDiffer(this, diffUtil)
+
+    var images: List<String>
+        get() = recyclerListDiffer.currentList
+        set(value) = recyclerListDiffer.submitList(value)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImageViewHolder {
+        val view = LayoutInflater.from(parent.context).inflate(R.layout.item_search_img, parent, false)
+        return ImageViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return images.size
+    }
+
+    fun setOnItemClickListener(listener: (String) -> Unit) {
+        onItemClickListener = listener
+    }
+
+    override fun onBindViewHolder(holder: ImageViewHolder, position: Int) {
+        val imageView = holder.itemView.findViewById<ImageView>(R.id.img_item_art)
+        val url = images[position]
+
+        holder.itemView.apply {
+            glide.load(url).into(imageView)
+            setOnItemClickListener {
+                onItemClickListener?.let {
+                    it(url)
+                }
+            }
+        }
+    }
+}
